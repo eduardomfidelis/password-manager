b9dba45fc81ffb0aa335b0e5e8feafc66e9806e1
eduardo <eduardomacielfidelis@gmail.com>
2023-07-11 20:25:24 -0300

    requisito(4)Valide os campos do formulário

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,19 +1,77 @@
+import { useState } from 'react';
+
+const initialFormValue = {
+  service: '',
+  login: '',
+  password: '',
+  url: '',
+};
 type FormProps = {
   cancelForm: () => void,
 };
+
 function Form({ cancelForm }: FormProps) {
+  const [formValue, setFormValue] = useState(initialFormValue);
+  const [formValidation, setFormValidation] = useState(false);
+  const { service, login, password, url } = formValue;
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormValue((prevFormValue) => ({
+      ...prevFormValue,
+      [name]: value,
+    }));
+    validateForm();
+  };
+
+  const validateForm = () => {
+    const isServiceValid = service.trim() !== '';
+    const isLoginValid = login.trim() !== '';
+    const isPasswordValid = password.length >= 8
+      && password.length <= 16
+      && /[a-zA-Z]/.test(password)
+      && /[0-9]/.test(password)
+      && /[!@#$%^&*]/.test(password);
+
+    setFormValidation(isServiceValid && isPasswordValid && isLoginValid);
+  };
+
   return (
     <div>
-      <label htmlFor="serviço">Nome do serviço</label>
-      <input type="text" id="serviço" />
+      <label htmlFor="service">Nome do serviço</label>
+      <input
+        type="text"
+        id="service"
+        name="service"
+        value={ service }
+        onChange={ handleInputChange }
+      />
       <label htmlFor="login">Login</label>
-      <input type="text" id="login" />
+      <input
+        type="text"
+        id="login"
+        name="login"
+        value={ login }
+        onChange={ handleInputChange }
+      />
       <label htmlFor="password">Senha</label>
-      <input type="password" id="password" />
+      <input
+        type="password"
+        id="password"
+        name="password"
+        value={ password }
+        onChange={ handleInputChange }
+      />
       <label htmlFor="url">URL</label>
-      <input type="text" id="url" />
+      <input
+        type="text"
+        id="url"
+        name="url"
+        value={ url }
+        onChange={ handleInputChange }
+      />
 
-      <button>Cadastrar</button>
+      <button disabled={ !formValidation }>Cadastrar</button>
       <button onClick={ cancelForm }>Cancelar</button>
     </div>
   );
