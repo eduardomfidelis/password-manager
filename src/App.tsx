29ceac6883a987c3bcdfd46ccd5988d03a2c6368
eduardo <eduardomacielfidelis@gmail.com>
2023-07-18 01:13:07 -0300

    requisito(8)  Implemente um checkbox para esconder/mostrar as senhas

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ type ServiceTipes = {
 function App() {
   const [showForm, setShowForm] = useState(false);
   const [services, setService] = useState<ServiceTipes[]>([]);
+  const [hidePasswords, setHidePasswords] = useState(false);
 
   const handleShowForm = () => {
     setShowForm(true);
@@ -24,7 +25,12 @@ function App() {
     setService([...services, service]);
     setShowForm(false);
   };
-
+  const handleRemoveService = (service: ServiceTipes) => {
+    setService((prevServices) => prevServices.filter((s) => s !== service));
+  };
+  const handleHidePassword = () => {
+    setHidePasswords(!hidePasswords);
+  };
   return (
     <div>
       <h1>Gerenciador de senhas</h1>
@@ -32,6 +38,14 @@ function App() {
         <>
           {services.length === 0 && <p>nenhuma senha cadastrada</p>}
           <button onClick={ handleShowForm }>Cadastrar nova senha</button>
+          <label htmlFor="hide-passwords">Esconder senhas</label>
+          <input
+            type="checkbox"
+            id="hide-passwords"
+            checked={ hidePasswords }
+            onChange={ handleHidePassword }
+          />
+
           {services.map((service) => (
             <div key={ service.service }>
               <a href={ service.url }>{service.service}</a>
@@ -43,8 +57,16 @@ function App() {
               <p>
                 Senha:
                 {' '}
-                {service.password}
+                {hidePasswords ? '******' : service.password}
               </p>
+              <button
+                type="button"
+                data-testid="remove-btn"
+                onClick={ () => handleRemoveService(service) }
+              >
+                Remover
+
+              </button>
             </div>
           ))}
         </>
